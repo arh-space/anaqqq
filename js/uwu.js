d6ccecf1c672ca375f0970f2cc9b3bf1cb86b583
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 04:12:32 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -39,10 +39,10 @@ const s = (p) => {
     const mid = fft.getEnergy("mid")
 
     const mapBass = p.map(bass, 0, 255, 0, 0.0)
-    const mapTremble = p.map(treble, 0, 255, 0, 10.0)
+    const mapTremble = p.map(treble, 0, 255, 0, 0.0)
     const mapMid = p.map(mid, 0, 255, 0.0, 0.1)
 
-    demo1Shader.setUniform('u_time', p.frameCount / 1000)
+    demo1Shader.setUniform('u_time', p.frameCount / 80000)
     demo1Shader.setUniform('u_bass', mapBass)
     demo1Shader.setUniform('u_tremble', mapTremble)
     demo1Shader.setUniform('u_mid', mapMid)
