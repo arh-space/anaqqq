4f0d513b1b7c585678b6273466c81c1242b17138
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 03:20:11 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -4,7 +4,7 @@ const s = (p) => {
   p.preload = () => {
     audio = p.loadSound('audio/uwu.mp3')
     demo1Shader = p.loadShader('shaders/base.vert', 'shaders/d1.frag')
-    img = p.loadImage('img/2.mp4')
+    img = p.loadImage('img/1.jpg')
   }
 
   p.setup = () => {
@@ -42,7 +42,7 @@ const s = (p) => {
     const mapTremble = p.map(treble, 0, 255, 0, 0.0)
     const mapMid = p.map(mid, 0, 255, 0.0, 0.1)
 
-    demo1Shader.setUniform('u_time', p.frameCount / 0)
+    demo1Shader.setUniform('u_time', p.frameCount / 3)
     demo1Shader.setUniform('u_bass', mapBass)
     demo1Shader.setUniform('u_tremble', mapTremble)
     demo1Shader.setUniform('u_mid', mapMid)
