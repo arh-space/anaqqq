bd7fed494d9fdbfac113c380426a1fed8cbb7197
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 01:19:10 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -1,10 +1,9 @@
 const s = (p) => {
-  let demo1Shader, img, fft, audio, toggleBtn
+  let video, fft, audio, toggleBtn
 
   p.preload = () => {
     audio = p.loadSound('audio/uwu.mp3')
-    demo1Shader = p.loadShader('shaders/base.vert', 'shaders/d1.frag')
-    img = p.loadImage('img/2.mp4')
+    video = p.loadVideo('img/2.mp4')
   }
 
   p.setup = () => {
@@ -22,38 +21,6 @@ const s = (p) => {
         toggleBtn.classList.toggle('toggle--on')
         this.toggleAudio()
       })
-
-      fft = new p5.FFT()
-      p.shader(demo1Shader)
-
-      demo1Shader.setUniform('u_resolution', [p.windowWidth, p.windowHeight])
-      demo1Shader.setUniform('u_texture', img)
-      demo1Shader.setUniform('u_tResolution', [img.width, img.height])
-  }
-
-  p.draw = () => {
-    fft.analyze()
-
-    const bass = fft.getEnergy("bass")
-    const treble = fft.getEnergy("treble")
-    const mid = fft.getEnergy("mid")
-
-    const mapBass = p.map(bass, 0, 255, 10, 15.0)
-    const mapTremble = p.map(treble, 0, 255, 0, 0.0)
-    const mapMid = p.map(mid, 0, 255, 0.0, 0.1)
-
-    demo1Shader.setUniform('u_time', p.frameCount / 20)
-    demo1Shader.setUniform('u_bass', mapBass)
-    demo1Shader.setUniform('u_tremble', mapTremble)
-    demo1Shader.setUniform('u_mid', mapMid)
-
-    p.rect(0,0, p.width, p.height)
-  }
-
-  p.windowResized = () => {
-    p.resizeCanvas(p.windowWidth, p.windowHeight)
-    demo1Shader.setUniform('u_resolution', [p.windowWidth, p.windowHeight])
-  }
 
   toggleAudio = () => {
     if (audio.isPlaying()) {
