1777d05096a574deb87744f1aae1e105ab6f905f
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 03:06:57 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -31,6 +31,22 @@ const s = (p) => {
       demo1Shader.setUniform('u_tResolution', [img.width, img.height])
   }
 
+    p.draw = () => {
+    fft.analyze()
+
+    const bass = fft.getEnergy("bass")
+    const treble = fft.getEnergy("treble")
+    const mid = fft.getEnergy("mid")
+
+    const mapBass = p.map(bass, 0, 255, 0, 0.0)
+    const mapTremble = p.map(treble, 0, 255, 0, 0.0)
+    const mapMid = p.map(mid, 0, 255, 0.0, 0.1)
+
+    demo1Shader.setUniform('u_time', p.frameCount / 0)
+    demo1Shader.setUniform('u_bass', mapBass)
+    demo1Shader.setUniform('u_tremble', mapTremble)
+    demo1Shader.setUniform('u_mid', mapMid)
+
   
     p.rect(0,0, p.width, p.height)
   }
