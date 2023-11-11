08b38ab34fa3b321f92153ec0a069af6aef5992f
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 01:58:59 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -1,9 +1,9 @@
 const s = (p) => {
-  let img, fft, audio, toggleBtn
+  let demo1Shader, img, fft, audio, toggleBtn
 
   p.preload = () => {
     audio = p.loadSound('audio/uwu.mp3')
-   
+    demo1Shader = p.loadShader('shaders/base.vert', 'shaders/d1.frag')
     img = p.loadImage('img/1.jpg')
   }
 
@@ -24,7 +24,28 @@ const s = (p) => {
       })
 
       fft = new p5.FFT()
-      
+      p.shader(demo1Shader)
+
+      demo1Shader.setUniform('u_resolution', [p.windowWidth, p.windowHeight])
+      demo1Shader.setUniform('u_texture', img)
+      demo1Shader.setUniform('u_tResolution', [img.width, img.height])
+  }
+
+  p.draw = () => {
+    fft.analyze()
+
+    const bass = fft.getEnergy("bass")
+    const treble = fft.getEnergy("treble")
+    const mid = fft.getEnergy("mid")
+
+    const mapBass = p.map(bass, 0, 255, 10, 15.0)
+    const mapTremble = p.map(treble, 0, 255, 0, 0.0)
+    const mapMid = p.map(mid, 0, 255, 0.0, 0.1)
+
+    demo1Shader.setUniform('u_time', p.frameCount / 20)
+    demo1Shader.setUniform('u_bass', mapBass)
+    demo1Shader.setUniform('u_tremble', mapTremble)
+    demo1Shader.setUniform('u_mid', mapMid)
 
     p.rect(0,0, p.width, p.height)
   }
