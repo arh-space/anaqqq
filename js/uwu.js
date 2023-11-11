43f610032c830cf979ff4df67a47610ca989ee89
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 01:57:40 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -1,1 +1,46 @@
+const s = (p) => {
+  let img, fft, audio, toggleBtn
 
+  p.preload = () => {
+    audio = p.loadSound('audio/uwu.mp3')
+   
+    img = p.loadImage('img/1.jpg')
+  }
+
+  p.setup = () => {
+      playBtn = document.querySelector('#play-btn')
+      playBtn.addEventListener('click', () => {
+        document.body.classList.add('start-anim')
+          audio.loop()
+      })
+
+      p.pixelDensity(1)
+      p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
+
+      toggleBtn = document.querySelector('#toggle-btn')
+      toggleBtn.addEventListener('click', () => {
+        toggleBtn.classList.toggle('toggle--on')
+        this.toggleAudio()
+      })
+
+      fft = new p5.FFT()
+      
+
+    p.rect(0,0, p.width, p.height)
+  }
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+    demo1Shader.setUniform('u_resolution', [p.windowWidth, p.windowHeight])
+  }
+
+  toggleAudio = () => {
+    if (audio.isPlaying()) {
+      audio.pause()
+    } else {
+      audio.loop()
+    }
+  }
+};
+
+new p5(s)
