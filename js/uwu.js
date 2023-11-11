f80ce655cfbde1ea34176924ef768556169602bf
ARH <65483182+arh-space@users.noreply.github.com>
2023-11-12 04:09:31 +0700

    Update uwu.js

--- js/uwu.js
+++ js/uwu.js
@@ -42,7 +42,7 @@ const s = (p) => {
     const mapTremble = p.map(treble, 0, 255, 0, 10.0)
     const mapMid = p.map(mid, 0, 255, 0.0, 0.1)
 
-    demo1Shader.setUniform('u_time', p.frameCount / 1)
+    demo1Shader.setUniform('u_time', p.frameCount / 1000)
     demo1Shader.setUniform('u_bass', mapBass)
     demo1Shader.setUniform('u_tremble', mapTremble)
     demo1Shader.setUniform('u_mid', mapMid)
